1b2cb15cc2b4277249b15095ed24020a40ed4ba6
signet <signet@emberfruit.com>
2016-01-30 00:28:26 +0100

    game timer for spawning rocks

--- src/states/PlayState.hx
+++ src/states/PlayState.hx
@@ -12,12 +12,19 @@ import flixel.util.FlxMath;
  */
 class PlayState extends FlxState
 {
+	public var dude:Dude; //Refers to the player's dude
+	public var rocks:FlxGroup; //A list of all the rocks
+	public var timer:Float; //A timer to decide when to spawn next rock
+	public var speedMultiplier:Float = 1; //Game speed
+	public var spawnMultiplier:Float = 2; //Frequency of rock spawning
+	
 	/**
 	 * Function that is called up when to state is created to set it up. 
 	 */
 	override public function create():Void
 	{
 		super.create();
+		rocks = new FlxGroup();
 		var playText = new FlxText(20, 20, 0, "You're now playing!");
 		playText.color = 0x111111;
 		add(playText);
@@ -37,6 +44,23 @@ class PlayState extends FlxState
 	 */
 	override public function update():Void
 	{
+
+		timer -= FlxG.elapsed * speedMultiplier;
+		if(timer <= 0)
+				spawnRock();
+				
 		super.update();
-	}	
+		FlxG.overlap(dude, rocks, hitRock);
+	}
+	
+	private function hitRock(dude:FlxObject, rock:FlxObject):Void
+	{
+		rock.kill();
+	}
+	
+	//This function resets the timer and adds a new rock to the game
+	private function spawnRock():Void
+	{
+		timer = 1+FlxG.random()*spawnMultiplier;	//Reset the timer
+	}
 }
